74838afce3a3685d7ce3424d9a88694e950c928f
Erika Fitzpatrick <erikafitz@Erikas-Air.home>
2015-07-06 22:06:54 -0400

    ios

---
+++ RIDE417/RIDE417/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RIDE417
+//
+//  Created by Erika Fitzpatrick on 7/6/15.
+//  Copyright (c) 2015 Erika Fitzpatrick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

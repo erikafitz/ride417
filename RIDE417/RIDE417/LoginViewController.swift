5ddd53a3ed9dfd3e083bf1640e7a1cdd06b456b7
Erika Fitzpatrick <erikafitz@Erikas-Air.home>
2015-07-28 10:42:05 -0400

    login

---
+++ RIDE417/RIDE417/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  RIDE417
+//
+//  Created by Erika Fitzpatrick on 7/28/15.
+//  Copyright (c) 2015 Erika Fitzpatrick. All rights reserved.
+//
+
+import Cocoa
+
+class LoginViewController: UIViewController {
+
+}
